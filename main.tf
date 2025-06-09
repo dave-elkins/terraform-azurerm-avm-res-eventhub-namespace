3f8008bc55c026dbd1d8098551f90f8054789111
Dave Elkins <148497092+dave-elkins@users.noreply.github.com>
2025-06-09 16:32:17 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -89,9 +89,10 @@ resource "azurerm_role_assignment" "this" {
 resource "azurerm_eventhub_namespace_customer_managed_key" "this" {
   count = var.customer_managed_key != {} ? 1 : 0
 
-  eventhub_namespace_id     = azurerm_eventhub_namespace.this[0].id
-  key_vault_key_ids         = [data.azurerm_key_vault_key.this[0].id]
-  user_assigned_identity_id = var.customer_managed_key.user_assigned_identity_resource_id
+  eventhub_namespace_id             = azurerm_eventhub_namespace.this[0].id
+  infrastructure_encryption_enabled = true
+  key_vault_key_ids                 = [data.azurerm_key_vault_key.this[0].id]
+  user_assigned_identity_id         = var.customer_managed_key.user_assigned_identity_resource_id
 }
 
 data "azurerm_key_vault_key" "this" {
