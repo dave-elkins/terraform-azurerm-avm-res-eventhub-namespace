0127c99bfe2fa5f9515fbe76860a3eea49f8139f
Dave Elkins <148497092+dave-elkins@users.noreply.github.com>
2025-06-09 16:26:04 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,3 +85,18 @@ resource "azurerm_role_assignment" "this" {
   skip_service_principal_aad_check       = each.value.skip_service_principal_aad_check
   delegated_managed_identity_resource_id = each.value.delegated_managed_identity_resource_id
 }
+
+resource "azurerm_eventhub_namespace_customer_managed_key" "this" {
+  count = var.customer_managed_key != {} : 1 : 0
+
+  eventhub_namespace_id     = azurerm_eventhub_namespace.this[0].id
+  key_vault_key_ids         = [azurerm_key_vault_key.this[0].id]
+  user_assigned_identity_id = var.customer_managed_key.user_assigned_identity_resource_id
+}
+
+data "azurerm_key_vault_key" "this" {
+  count = var.customer_managed_key != {} : 1 : 0
+
+   key_vault_id = var.customer_managed_key.key_vault_resource_id
+   name         = var.customer_managed_key.key_name
+}
